b4471057311e9f1f6a4fd65fc6594dd34ea74932
fidele <fidelniyomugabo67@gmail.com>
2025-04-09 11:45:12 +0200

    add:basics of dart

---
+++ variables_and_data_types.dart
@@ -0,0 +1,9 @@
+void main() {
+  int age = 25;
+  double price = 99.99;
+  String name = 'John';
+  bool isAlive = true;
+  dynamic anything = 'Can be any type';
+
+  print('$name is $age years old. Price: \$${price}');
+}
